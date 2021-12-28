7c25cad6434cce36784cbfd545326129cc969f37
zyndel <zyndel94@gmail.com>
2021-12-28 11:51:40 +0300

    work in progress

--- Task_1.py
+++ Task_1.py
@@ -2,17 +2,26 @@ import argparse
 import json
 import xml.etree.cElementTree as ET
 
-parser = argparse.ArgumentParser()
-parser.add_argument('rooms', type=str, help='This will be name of input rooms file')
-parser.add_argument('students', type=str, help='This will be name of input students file')
-parser.add_argument('st_in_rooms', type=str, help='This will be name of output result file')
-args = parser.parse_args()
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("rooms", type=str, help="This will be name of input rooms file")
+    parser.add_argument(
+        "students", type=str, help="This will be name of input students file"
+    )
+    parser.add_argument(
+        "st_in_rooms", type=str, help="This will be name of output result file"
+    )
+    return parser.parse_args()
 
 
 class Student:
     def __init__(self, student_id, name):
         self.id = student_id
         self.name = name
+
+    def __repr__(self):
+        return f"<Student {self.id} {self.name}>"
 
 
 class Room:
@@ -21,92 +30,119 @@ class Room:
         self.name = name
         self.students = students
 
+    def __repr__(self):
+        return f"<Room {self.id} {self.name} {self.students}>"
+
 
 def bl(data_about_rooms, data_about_students):
-    rooms_for_students = {key: [] for key in range(len(data_about_rooms))}
+    students_by_room = {}
+    for el in data_about_rooms:
+        key = el["id"]
+        students_by_room[key] = []
+    # students_by_room = {key: [] for key in range(len(data_about_rooms))}
+
     for evidence_of_student in data_about_students:
         room = evidence_of_student["room"]
         student = Student(evidence_of_student["id"], evidence_of_student["name"])
-        rooms_for_students[room].append(student)
+        students_by_room[room].append(student)
 
     students_in_rooms = []
     for evidence_of_room in data_about_rooms:
         room_id = evidence_of_room["id"]
-        room = Room(evidence_of_room["id"], evidence_of_room["name"], rooms_for_students[room_id])
+        room = Room(
+            evidence_of_room["id"], evidence_of_room["name"], students_by_room[room_id]
+        )
         students_in_rooms.append(room)
     return students_in_rooms
 
 
-def json_serializer():
+def json_serializer(args):
+    # read rooms file. maybe xml
     with open(args.rooms, "r") as rooms_file:
         data_about_rooms = json.load(rooms_file)
+    # read students file. maybe xml
     with open(args.students, "r") as students_file:
         data_about_students = json.load(students_file)
 
+    # group students by rooms, resulting in a list of rooms
     students_in_rooms = bl(data_about_rooms, data_about_students)
 
-    result = [{}, {}]
+    # prepare result for json result
+    result = []
     for room in students_in_rooms:
+        student_dicts = [dict(id=s.id, name=s.name) for s in room.students]
         item = {
             "id": room.id,
+            "name": room.name,
+            "students": student_dicts,
         }
         result.append(item)
 
+    # save result to json, maybe xml
     with open(args.st_in_rooms, "w") as file:
         json.dump(result, file, indent=4, ensure_ascii=False)
 
 
-# def xml_serializer():
-#     students_xml_tree = ET.parse("students.xml")
-#     """
-#     <xml>
-#       <student id=12312 name="gratest mind ever" room=105 />
-#       ....
-#     </xml>
-#     """
-#     data_about_students = [child.attrib for child in students_xml_tree]
-#
-#     rooms_xml_tree = ET.parse("rooms.xml")
-#     """
-#     <xml>
-#       <room id=12312 name="gratest mind ever" />
-#       ....
-#     </xml>
-#     """
-#     data_about_rooms = []
-#     for child in rooms_xml_tree:
-#         data_about_rooms.append(child.attrib)
-#
-#     students_by_room = bl(data_about_students, data_about_rooms)
-#
-#     rooms = ET.Element("rooms")
-#     room = ET.SubElement(rooms, "room")
-#     ET.SubElement(room, "room_id")
-#     ET.SubElement(room, "room_name")
-#     students = ET.SubElement(room, "students")
-#     ET.SubElement(students, "student_id")
-#     ET.SubElement(students, "student_name")
-#
-#     tree = ET.ElementTree(rooms)
-#     tree.write("filename.xml")
+def xml_serializer(args):
+    students_xml_tree = ET.parse(args.students)
+    """
+    <xml>
+      <student id=12312 name="gratest mind ever" room=105 />
+      ....
+    </xml>
+    """
+    root = students_xml_tree.getroot()
+    data_about_students = [child.attrib for child in root]
 
-#
-#
-# while True:
-#     user_answer = input("Choose output file's type: json(1) or xml(2)")
-#     try:
-#         user_answer = int(user_answer)
-#     except ValueError:
-#         continue
-#     else:
-#         if user_answer == 1 or user_answer == 2:
-#             break
-#         else:
-#             continue
-#
-# if user_answer == 1:
-#     json_serializer()
-# else:
-#     xml_serializer()
+    rooms_xml_tree = ET.parse(args.rooms)
+    """
+    <xml>
+      <room id=12312 name="gratest mind ever" />
+      ....
+    </xml>
+    """
+    root = rooms_xml_tree.getroot()
+    data_about_rooms = [child.attrib for child in root]
 
-json_serializer()
+    students_by_room = bl(data_about_rooms, data_about_students)
+
+    """
+    <xml>
+      <room id=12312 name="gratest mind ever">
+        <student id="567" name="ololo" />
+        ...
+      </room>
+      ...
+    </xml>
+    """
+    root = ET.Element("xml")
+    for r in students_by_room:
+        room = ET.SubElement(root, "room", attrib={"id": r.id, "name": r.name})
+        for st in r.students:
+            student = ET.SubElement(
+                room, "student", attrib={"id": st.id, "name": st.name}
+            )
+
+        tree = ET.ElementTree(root)
+        tree.write(args.st_in_rooms)
+
+
+def main(args):
+    if (
+        args.students.endswith(".json")
+        and args.rooms.endswith(".json")
+        and args.st_in_rooms.endswith(".json")
+    ):
+        json_serializer(args)
+
+    if (
+        args.students.endswith(".xml")
+        and args.rooms.endswith(".xml")
+        and args.st_in_rooms.endswith(".xml")
+    ):
+        xml_serializer(args)
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
